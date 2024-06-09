479012406c75490ea29718591cbce071db24cb15
Danh Nguyen <ncdanh2204@gmail.com>
2024-06-09 13:13:17 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const app = express();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
